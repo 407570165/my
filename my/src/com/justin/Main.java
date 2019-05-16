3893bd21753a639bb7c77b1d8b73ec1142c6a504
407570165 <justinchu0304@gmail.com>
2019-05-16 16:52:07 +0800

    5/16

---
+++ my/src/com/justin/Main.java
@@ -0,0 +1,9 @@
+package com.justin;
+
+import java.io.FileInputStream;
+
+public class Main {
+	public static void main(String[] args)throws Exception {
+	FileInputStream fis =new FileInputStream("result.txt");
+	}
+}
